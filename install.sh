2123a15e67c9216cebad1102b7e65e960282ad0f
zerg <anonzerg@proton.me>
2024-05-24 18:38:54 +0330

    bash script to install and update required packages.

---
+++ install.sh
@@ -0,0 +1,34 @@
+#! /usr/bin/env bash
+
+apt-get update && apt-get upgrade -y
+
+if [[ $? -eq 0 ]]; then
+  echo "update and upgrade succeeded"
+else
+  echo "update and upgrade process exit with $?."
+  echo "update and upgrade failed."
+fi
+
+apt-get install \
+  vim \
+  neovim \
+  vlc \
+  clang \
+  thunderbird \
+  zsh \
+  alacritty \
+  libgsl-dev \
+  ruby \
+  manpages-posix \
+  manpages-posix-dev \
+  moreutils \
+  gdb \
+  gfortran \
+  tmux -y
+
+if [[ $? -eq 0 ]]; then
+  echo "required packages installed"
+else
+  echo "packages installation process exit with $?."
+  echo "packages installation failed."
+fi
